8b93bcf89523ae35ddca0d00156e9a7c0cc7f7e8
Matt Gatner <mgatner@tattersoftware.com>
2019-09-11 20:44:23 +0000

    Remove prefix contraint option (too buggy)

--- bin/Schemas.php
+++ bin/Schemas.php
@@ -12,6 +12,12 @@
 
 class Schemas extends \Tatter\Schemas\Config\Schemas
 {
-	// whether to continue instead of throwing exceptions
+	// Whether to continue instead of throwing exceptions
 	public $silent = true;
+	
+	// Whether to ignore the migrations table
+	public $ignoreMigrationsTable = true;
+	
+	// Default time-to-live for a stored schema (e.g. Cache)
+	public $ttl = 6000;
 }
