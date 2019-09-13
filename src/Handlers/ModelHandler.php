1bf0953e09dd1b68b337177656443c606303df40
Matt Gatner <mgatner@tattersoftware.com>
2019-09-13 13:37:59 +0000

    First pass on ModelHandler

--- src/Handlers/ModelHandler.php
+++ src/Handlers/ModelHandler.php
@@ -68,16 +68,65 @@ class ModelHandler extends BaseHandler implements SchemaHandlerInterface
 	public function import(): ?Schema
 	{
 		$schema = new Schema();
-		//availalbe model properties: 'primaryKey', 'table', 'returnType', 'DBGroup'
+
 		foreach ($this->getModels() as $class)
 		{
-			// Instantiate the model to harvest its properties
-			$model = new $class();
+			// Instantiate the model
+			$instance = new $class();
+
+			// Harvest model attributes from the instance
+			$model = new stdClass();
+
+			// Visible model attributes
+			foreach (['primaryKey', 'table', 'returnType', 'DBGroup'] as $attribute)
+			{
+				$model->$attribute = $instance->$attribute;
+			}
 			
+			// Protected attributes
+			foreach (['allowedFields', 'useSoftDeletes', 'useTimestamps',
+				'createdField', 'updatedField', 'deletedField', 'dateFormat'] as $attribute)
+			{
+				$model->$attribute = $this->getPrivateProperty($instance, $attribute);
+			}
+			unset($instance);
+
 			// Start a new table
-			$table = new Table($model->table);
-		
-			$schema->tables[$table->name] = $table;
+			$table             = new Table($model->table);
+			$table->model      = $class;
+			$table->returnType = $model->returnType;
+			
+			// Create a field for the primary key
+			$field = new Field($model->primaryKey);
+			$field->primary_key = true;
+			$table->fields->{$field->name} = $field;
+			
+			// Create a field for each allowedField
+			foreach ($model->allowedField as $fieldName)
+			{
+				$field = new Field($fieldName);
+				$table->fields->$fieldName = $field;
+			}
+			
+			// Figure out which timestamp fields (if any) this model uses and add them
+			$timestamps = $model->useTimestamps ? ['createdField', 'updatedField'] : [];
+			if ($model->useSoftDeletes)
+			{
+				$timestamps[] = 'deletedField';
+			}
+			
+			// Get field names from each timestamp attribute
+			foreach ($timestamps as $attribute)
+				{
+					$fieldName = $model->$attribute;
+					$field = new Field($fieldName);
+					$field->type = $model->dateFormat;
+					
+					$table->fields->$fieldName = $field;
+				}
+			}
+			
+			$schema->tables->{$table->name} = $table;
 		}
 		
 		return $schema;
